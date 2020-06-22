d9893d81e1e70528eefc262f4d31391b893b05b2
Luiz Anthero Gama <luizanthero@icloud.com>
2020-06-22 18:57:11 -0300

    adding menus

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './components';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent],
 })
 export class SharedModule {}
