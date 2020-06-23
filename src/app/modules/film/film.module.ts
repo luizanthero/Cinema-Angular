2773912cf9f5634868bea35441a2bc9c0692245a
Luiz Anthero Gama <luizanthero@icloud.com>
2020-06-23 11:47:34 -0300

    adding service on module film

--- src/app/modules/film/film.module.ts
+++ src/app/modules/film/film.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
+import { FilmRoutingModule } from './film-routing.module';
 
+import { SharedModule } from 'src/app/shared';
+
+import { FilmService } from './service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, HttpClientModule, SharedModule, FilmRoutingModule],
+  providers: [FilmService],
 })
-export class FilmModule { }
+export class FilmModule {}
