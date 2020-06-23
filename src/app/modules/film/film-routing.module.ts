4e2bd4955538e1035bb8defdeb60384caef99e40
Luiz Anthero Gama <luizanthero@icloud.com>
2020-06-23 10:59:43 -0300

    adding module film

---
+++ src/app/modules/film/film-routing.module.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+//import { SelectExhibitionComponent } from './components';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class FilmRoutingModule {}
--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,1 +1,2 @@
 export * from './exhibition';
+export * from './film';
