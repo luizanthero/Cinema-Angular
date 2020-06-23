25e078f00072d1f2737447da8a22315b90c9198e
Luiz Anthero Gama <luizanthero@icloud.com>
2020-06-23 10:30:47 -0300

    adding default footer in application

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { NavbarComponent } from './components';
+import { NavbarComponent, FooterComponent } from './components';
 import { OmdbService } from './services';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, FooterComponent],
   providers: [OmdbService],
 })
 export class SharedModule {}
