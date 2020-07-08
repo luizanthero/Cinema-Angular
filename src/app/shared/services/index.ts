bbed75caf138dd81091e26b725fc2868a498b62a
Luiz Anthero Gama <luizanthero@icloud.com>
2020-07-08 19:25:24 -0300

    adding interceptor loader

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,1 +1,2 @@
 export * from './omdb.service';
+export * from './loader.service';
---
+++ src/app/shared/services/loader.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoaderService {
+  constructor() {}
+
+  isLoading = new BehaviorSubject(false);
+}
