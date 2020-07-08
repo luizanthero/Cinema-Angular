0ec32d231dfcb113f109e55921510a971d205e9c
Luiz Anthero Gama <luizanthero@icloud.com>
2020-07-08 17:21:08 -0300

    ajusting footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  message: string;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const year = new Date().getFullYear();
+    const name = 'Luiz Anthero Gama';
+
+    this.message = `Developing by ${name}, ${year}`;
+  }
 }
