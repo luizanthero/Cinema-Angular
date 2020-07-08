c92df1dd5a847b819dcc58f87abeead46e0ce817
Luiz Anthero Gama <luizanthero@icloud.com>
2020-07-08 19:27:57 -0300

    ajusting loading component

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoaderService } from '../../services';
+
 @Component({
   selector: 'cine-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.css'],
 })
 export class LoadingComponent implements OnInit {
-  constructor() {}
+  loading: boolean;
+
+  constructor(private loader: LoaderService) {
+    this.loader.isLoading.subscribe((response) => {
+      console.log(response);
+      this.loading = response;
+    });
+  }
 
   ngOnInit(): void {}
 }
