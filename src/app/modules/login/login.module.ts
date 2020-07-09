d61bb609e8a26e287625320363e5cec0749f6685
Luiz Anthero Gama <luizanthero@icloud.com>
2020-07-09 14:24:01 -0300

    adding component authenticate in Login Module

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthenticationComponent } from './components';
+import { HttpClientModule } from '@angular/common/http';
 
+import { LoginRoutingModule } from './login-routing.module';
+
+import { SharedModule } from 'src/app/shared';
+
+import { LoginService } from './service';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AuthenticationComponent],
+  imports: [CommonModule, HttpClientModule, SharedModule, LoginRoutingModule],
+  providers: [LoginService],
 })
-export class LoginModule { }
+export class LoginModule {}
