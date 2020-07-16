77440f108d41d23baacc013e20bfa269b38f87d0
Luiz Anthero Gama <luizanthero@icloud.com>
2020-07-16 18:07:13 -0300

    ajusting bug in paginator buttons

--- src/app/shared/components/table-dynamic/table-paginator/table-paginator.component.ts
+++ src/app/shared/components/table-dynamic/table-paginator/table-paginator.component.ts
@@ -42,10 +42,10 @@ export class TablePaginatorComponent implements OnChanges {
     if (this.isPaginate) {
       if (this.dataLength > this.dataSource.filteredData.length) {
         if (this.pageIndex <= event.pageIndex) {
-          this.skip = this.skip + 1;
+          this.skip = event.pageIndex + 1;
           this.paginatorAction.emit(this.skip);
         } else if (this.pageIndex > event.pageIndex) {
-          this.skip = this.skip - 1;
+          this.skip = event.pageIndex + 1;
           this.paginatorAction.emit(this.skip);
         }
       }
